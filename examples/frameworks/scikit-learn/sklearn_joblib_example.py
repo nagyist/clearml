0ce0914e692f824b210f42d01f4148006c0030c4
clearml <>
2026-01-25 17:42:42 +0200

    Fix default regression params

--- examples/frameworks/scikit-learn/sklearn_joblib_example.py
+++ examples/frameworks/scikit-learn/sklearn_joblib_example.py
@@ -22,7 +22,7 @@ y = iris.target
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-model = LogisticRegression(solver='liblinear', multi_class='auto')  # sklearn LogisticRegression class
+model = LogisticRegression(solver='lbfgs', max_iter=1000)  # sklearn LogisticRegression class
 model.fit(X_train, y_train)
 
 joblib.dump(model, 'model.pkl', compress=True)
